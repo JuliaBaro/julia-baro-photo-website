7e58ecc6960a4385ff822d8cd89553bbc3481461
unknown <baro.julia@gmail.com>
2020-06-17 09:44:18 +0200

    css layout (broke) with some content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-//import line from './line.svg';
+import line from './line.svg';
 //import leftArrow from './leftArrow';
 //import rightArrow from './rightArrow';
 //import quotemark from './quotemark';
@@ -31,89 +31,110 @@ function App() {
   return (
     <div className="app_container">
 
+
+      
       <div className="navigation">
-        navigation
+        <div className="">
+          <img alt="img1"/>
+          <img alt="img2"/>
+          <img alt="img3"/>
+          <img alt="img4"/>
+        </div>
+        <h3>művészi portré és párfotó</h3>
+        <h3>— Budapest —</h3>
       </div>
 
-      <div className="philosophy">
-        philosophy
-        {/*<img alt="philosophy img"/>
-        <h1>Látásmódom</h1>
-        <h2>Őszinte képvilág</h2>
-        <p>Kíváncsi természetű vagyok, örök megfigyelő. Elbűvöl az emberek sokfélesége. Meggyőződésem, 
-          hogy mindenkiről lehet jó képet készíteni. Hogy mitől jó számomra egy fotó? Néhány szóban: spontán, 
-          őszinte, egyszerű. Minden mesterkéltségtől és álarcoktól mentes. Olyan, amelyen a fotózott igaz 
-          önmagát látja viszont.
-        </p>
-        <h2>Tudj meg többet</h2>
-        <img src={line} className="divLine" alt="line" />*/}
+      <div className="philosophy"> {/*rename div to section for seo?*/}
+        <div> {/*embedded css grid?*/}
+          <img alt="philosophy img"/>
+          <h1>Látásmódom</h1>
+          <h2>Őszinte képvilág</h2>
+          <p>Kíváncsi természetű vagyok, örök megfigyelő. Elbűvöl az emberek sokfélesége. Meggyőződésem, 
+            hogy mindenkiről lehet jó képet készíteni. Hogy mitől jó számomra egy fotó? Néhány szóban: spontán, 
+            őszinte, egyszerű. Minden mesterkéltségtől és álarcoktól mentes. Olyan, amelyen a fotózott igaz 
+            önmagát látja viszont.
+          </p>
+          <h2>Tudj meg többet</h2>
+          <img src={line} className="divLine" alt="line" />
+        </div>
       </div>
 
       <div className="newestWork">
-        newestWork
-        {/*<h1>Legújabb képeim</h1>
+        <h1>Legújabb képeim</h1>
         <h2>Válogatott kedvencek</h2>
         <img alt="img1"/>
         <img alt="img2"/>
         <img alt="img3"/>
         <img alt="img4"/>
         <img alt="img5"/>
-        <h2>Összes galéria</h2>*/}
+        <h2>Összes galéria</h2>
       </div>
 
       <div className="categories">
-        categories
-        {/*<img src={line} className="divLine" alt="line" />
-        <h1>Portré</h1>
-        <h2>Tovább</h2>
-        <h1>Párfotó</h1>
-        <h2>Tovább</h2>
-        <h1>Divatfotó</h1>
-        <h2>Tovább</h2>
-      <img src={line} className="divLine" alt="line" />*/}
+        <img src={line} className="divLine" alt="line" />
+        <div className="categories_flex">
+          <div /*className="categories_flex_item"*/>
+            <h1>Portré</h1>
+            <h2>Tovább</h2>
+          </div>
+          <div /*className="categories_flex_item"*/>
+            <h1>Párfotó</h1>
+            <h2>Tovább</h2>
+          </div>
+          <div /*className="categories_flex_item"*/>
+            <h1>Divatfotó</h1>
+            <h2>Tovább</h2>
+          </div>
+        </div>
+      <img src={line} className="divLine" alt="line" />
       </div>
 
       <div className="testimonials">
-        testimonials
-        {/*<img alt="testimonial img"/>
+        <img alt="testimonial img"/>
           <h1>Vélemények</h1>
         <p>Felülírtad eddigi fotózással kapcsolatos élményeimet, amiért nagyon hálás vagyok! Egy nagyon kellemes nappal 
           és sok őszinte képpel lettem gazdagabb (amiket szívesen nézek vissza). Szandra
-        </p>*/}
+        </p>
       </div>
 
       {/*<footer>*/}
       <div className="followOnInsta">
-        followOnInsta
-        {/*<img src={line} className="divLine" alt="line" />
+        <img src={line} className="divLine" alt="line" />
         <h1>Kövess az Instagramon</h1> 
         <h2>@juliabarophoto</h2>
-        <img alt="insta img1"/>
-        <img alt="insta img2"/>
-        <img alt="insta img3"/>
-        <img alt="insta img4"/>
-        <img alt="insta img5"/>
-        <img alt="insta img6"/>*/}
+        <div className="insta_photos_flex">
+          <img alt="insta img1"/>
+          <img alt="insta img2"/>
+          <img alt="insta img3"/>
+          <img alt="insta img4"/>
+          <img alt="insta img5"/>
+          <img alt="insta img6"/>
+        </div>
       </div>
 
       <div className="footerBrowser">
-        footerBrowser
-        {/*<h1>Kereső</h1>
-        <h2>Galériák</h2>
-        <h2>Infó</h2>
-        <h2>Rólam</h2>
-        <h2>Kapcsolat</h2>  
-        <img alt="portrait"/>
-        <h1>Kapcsolat</h1>
-        <p>+36 30 224 5167</p>*/}
+        <div className="footer_flex">
+          <div>
+            <h1>Kereső</h1>
+            <h2>Galériák</h2>
+            <h2>Infó</h2>
+            <h2>Rólam</h2>
+            <h2>Kapcsolat</h2>  
+          </div>
+          <div>
+            <img alt="bottom-logo"/>
+          </div>
+          <div>
+            <h1>Kapcsolat</h1>
+            <p>+36 30 224 5167</p>
+          </div>
+        </div>
       </div>
 
       <div className="copyright">
-        copyright
-        {/*<h2>© 2020 Julia Baro Photography</h2>*/}
+        <h4>© 2020 Julia Baro Photography</h4>
       </div>
       {/*</footer>*/}
-
     </div>
   );
 }
