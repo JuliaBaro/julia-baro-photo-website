ac7c47400d9c717c66f337fcb6dae600d757b438
unknown <baro.julia@gmail.com>
2020-07-15 15:23:25 +0200

    defining fonts and adding images to landing

--- src/App.js
+++ src/App.js
@@ -26,8 +26,17 @@ function App() {
 
       {/* ACCESS IMAGES IN PUBLIC FOLDER */}
       {/* https://stackoverflow.com/questions/47196800/reactjs-and-images-in-public-folder */}
-
       {/* png vs jpg format imgs? */}
+      {/*** use image attributes like title, define language of naming https://imageseo.io/image-seo-alt-title-name-figcaption/*/}
+      {/*** Use the same language in alt tag what you are using for 
+      your page content. Obviously you don't want Arabic 
+      language to show-up when image is missing; if your default page language is English. */}
+      {/* https://www.seroundtable.com/google-do-not-translate-image-file-names-28903.html 
+      In short, do not translate the image filename if the same image is being used elsewhere. Keep that 
+      filename and URL for the image so Google does not need to consolidate the images on their end.
+      */}
+      {/*** create favicon */}
+
       
       <div className="navigation">
         <div className="logo">
@@ -38,6 +47,9 @@ function App() {
         <img src={line} className="divLine" alt="line" />
 
         {/* navigation base: https://codepen.io/juliabaro/pen/JjGajvW */}
+        {/* https://codepen.io/deyand/pen/pvmyjw */}
+        {/* https://codepen.io/mzeenkala/pen/gBmwob */}
+        {/*** add searchbox? */}
         <nav class="navbar">
             <div class="menu-toggle" id="mobile-menu">
             <span class="bar"></span>
@@ -64,7 +76,7 @@ function App() {
 
       <div className="philosophy"> {/*rename div to section for seo?*/}
         <div> {/*embedded css grid?*/}
-          <img alt="philosophy img"/>
+          <img src={process.env.PUBLIC_URL + "/images/black-and-white-portrait.png"} alt="Black and white portrait of young lady" />
           <h1>Látásmódom</h1>
           <h2>Őszinte képvilág</h2>
           <p>Kíváncsi természetű vagyok, örök megfigyelő. Elbűvöl az emberek sokfélesége. Meggyőződésem, 
@@ -72,7 +84,7 @@ function App() {
             őszinte, egyszerű. Minden mesterkéltségtől és álarcoktól mentes. Olyan, amelyen a fotózott igaz 
             önmagát látja viszont.
           </p>
-          <h2>Tudj meg többet</h2>
+          <h5>Tudj meg többet</h5>
           <img src={line} className="divLine" alt="line" />
         </div>
       </div>
@@ -80,26 +92,26 @@ function App() {
       <div className="newestWork">
         <h1>Legújabb képeim</h1>
         <h2>Válogatott kedvencek</h2>
-        <img alt="img1"/>
-        <img alt="img2"/>
-        <img alt="img3"/>
-        <img alt="img4"/>
-        <img alt="img5"/>
-        <h2>Összes galéria</h2>
+        <img src={process.env.PUBLIC_URL + "/images/man-portrait.png"} alt="Young man portrait" />
+        <img src={process.env.PUBLIC_URL + "/images/couple-portrait.png"} alt="Kissing couple portrait" />
+        <img src={process.env.PUBLIC_URL + "/images/fashion-portrait.png"} alt="Closeup fashion portrait" /> {/*** change this pic */}
+        <img src={process.env.PUBLIC_URL + "/images/lifestyle-photo.png"} alt="Black and white portrait of spinning girl" />
+        <img src={process.env.PUBLIC_URL + "/images/closeup-portrait.png"} alt="Portrait of daydreaming girl" />
+        <h5>Összes galéria</h5>
       </div>
 
       <div className="categories">
         <img src={line} className="divLine" alt="line" />
         <div className="categories_flex">
-          <div /*className="categories_flex_item"*/>
+          <div /* className="categories_flex_item" */>
             <h1>Portré</h1>
             <h2>Tovább</h2>
           </div>
-          <div /*className="categories_flex_item"*/>
+          <div /* className="categories_flex_item" */>
             <h1>Párfotó</h1>
             <h2>Tovább</h2>
           </div>
-          <div /*className="categories_flex_item"*/>
+          <div /* className="categories_flex_item"   */>
             <h1>Divatfotó</h1>
             <h2>Tovább</h2>
           </div>
@@ -108,30 +120,33 @@ function App() {
       </div>
 
       <div className="testimonials">
-        <img alt="testimonial img"/>
-          <h1>Vélemények</h1>
+      portrait-with-glasses
+        <img src={process.env.PUBLIC_URL + "/images/portrait-with-glasses.png"} alt="Red haired girl portrait with glasses" />
+        <h1>Vélemények</h1>
         <p>Felülírtad eddigi fotózással kapcsolatos élményeimet, amiért nagyon hálás vagyok! Egy nagyon kellemes nappal 
           és sok őszinte képpel lettem gazdagabb (amiket szívesen nézek vissza). Szandra
         </p>
       </div>
 
-      {/*<footer>*/}
+      {/* <footer> */}
       <div className="followOnInsta">
         <img src={line} className="divLine" alt="line" />
         <h1>Kövess az Instagramon</h1> 
         <h2>@juliabarophoto</h2>
+        {/*** is it possible to embed photos from insta */}
         <div className="insta_photos_flex">
-          <img alt="insta img1"/>
-          <img alt="insta img2"/>
-          <img alt="insta img3"/>
-          <img alt="insta img4"/>
-          <img alt="insta img5"/>
-          <img alt="insta img6"/>
+          <img src={process.env.PUBLIC_URL + "/images/insta-portrait-in-nature-1.png"} alt="Moody portrait in nature" />
+          <img src={process.env.PUBLIC_URL + "/images/insta-red-portrait-2.png"} alt="Red haired girl portrait" />
+          <img src={process.env.PUBLIC_URL + "/images/insta-black-and-white-3.png"} alt="Black and white portrait of reading man" />
+          <img src={process.env.PUBLIC_URL + "/images/insta-hand-details-4.png"} alt="Fashion photo hand details" />
+          <img src={process.env.PUBLIC_URL + "/images/insta-closeup-portrait-5.png"} alt="Fashion portrait with scarf" />
+          <img src={process.env.PUBLIC_URL + "/images/insta-musician-portrait-6.png"} alt="Musician portrait in monochrome" />
         </div>
       </div>
 
+      {/*** add back to top arrow? */}
       <div className="footerBrowser">
-        <div className="footer_flex">
+        <div className="footer_flex"> 
           <div>
             <h1>Kereső</h1>
             <h2>Galériák</h2>
@@ -140,11 +155,11 @@ function App() {
             <h2>Kapcsolat</h2>  
           </div>
           <div>
-            <img alt="bottom-logo"/>
+            <img src={process.env.PUBLIC_URL + "/images/bottom-logo.png"} alt="Julia Baro Photography Logo" />
           </div>
           <div>
             <h1>Kapcsolat</h1>
-            <p>+36 30 224 5167</p>
+            <h6>+36 30 224 5167</h6>
           </div>
         </div>
       </div>
@@ -152,26 +167,10 @@ function App() {
       <div className="copyright">
         <h4>© 2020 Julia Baro Photography</h4>
       </div>
-      {/*</footer>*/}
+      {/* </footer> */}
     </div>
   );
 }
 
 export default App;
 
-//footer should be a separate component
-//images should be embeded from insta
-
-//COMPONENTS:
-//nav
-//galeriak
-//galeria
-//info
-//rolam
-//kapcsolat
-//footer
-
-//<Info />
-//<About />
-//<Contact />
-//<Footer />
